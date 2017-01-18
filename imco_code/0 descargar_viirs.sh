53b720e28e22189fc3e1b2258b2087b3cc60f113
Diego-MX <diego@opi.la>
2017-01-18 11:11:13 -0600

    Sólo una pasadita a los archivos necesarios.

--- imco_code/0 descargar_viirs.sh
+++ imco_code/0 descargar_viirs.sh
@@ -9,10 +9,11 @@
 
 direccion=$1
 # https://data.ngdc.noaa.gov/instruments/remote-sensing/passive/
-#   spectrometers-radiometers/imaging/viirs/dnb_composites/v10//201412/
-#   vcmslcfg/SVDNB_npp_20141201-20141231_75N180W_vcmslcfg_v10_c201502231126.tgz
+#   spectrometers-radiometers/imaging/viirs/dnb_composites/v10//201412/vcmcfg/
+#   SVDNB_npp_20141201-20141231_75N180W_vcmcfg_v10_c201502231125.tgz
 
-# direccion=https://data.ngdc.noaa.gov/instruments/remote-sensing/passive/spectrometers-radiometers/imaging/viirs/dnb_composites/v10//201412/vcmslcfg/SVDNB_npp_20141201-20141231_75N180W_vcmslcfg_v10_c201502231126.tgz
+
+# direccion=https://data.ngdc.noaa.gov/instruments/remote-sensing/passive/spectrometers-radiometers/imaging/viirs/dnb_composites/v10//201412/vcmcfg/SVDNB_npp_20141201-20141231_75N180W_vcmcfg_v10_c201502231125.tgz
 
 # nombre_largo=compositor_satelite_fechas_region_config_version_creado.extension
 # Se puede dividir con el siguiente comando.  
@@ -43,8 +44,9 @@ rename "s/$nombre_0/$nombre_corto/" ../data/viirs/raw_raster/unzipped/*
 
 gdalwarp -q -cutline ../data/inegi/marco_geo/raw/areas_geoestadisticas_estatales.shp \
   -crop_to_cutline --config GDALWARP_IGNORE_BAD_CUTLINE YES \
-  "../data/viirs/raw_raster/unzipped/"$nombre_corto".cf_cvg.tif" \
-  "../data/viirs/raw_raster/final/"$nombre_corto".cf_cvg.tif"
+  "../data/viirs/raw_raster/unzipped/"$nombre_corto".avg_rade9.tif" \
+  "../data/viirs/raw_raster/final/"$nombre_corto".avg_rade9.tif"
+
 
 
 
--- imco_code/5 comprobar_porcentajes.R
+++ imco_code/5 comprobar_porcentajes.R
@@ -10,18 +10,15 @@
 #   act_eco del estado
 #   act_eco de las ciudades del estado
 
+library(RColorBrewer)
 
 metros_ref <- read_csv("zonas_metro_estado_ok.csv" %>% 
     file.path("../data/referencias", .)) %>% 
     select(CVEMET, zona_metro = nombre_corto, CVEMUN, CVEENT)
 
-muns_ae <- read_csv("por_municipio.csv" %>% 
-    file.path("../data/resultados/integrado", .)) %>%
-  select(trimestre, CVEMUN, acteco) %>% 
-  filter(CVEMUN %in% metros_ref$CVEMUN)
-  
-metros_ae <- read_csv("por_zonas_metro.csv" %>% 
-  file.path("../data/resultados/integrado", .)) %>% 
+metros_ae <- read_csv("selecto_zm_edo.csv" %>% 
+  file.path("../data/resultados/integrado", .), 
+  col_types = "cccDnn") %>% 
   select(trimestre, CVEENT, CVEMET, zona_metro, acteco)
 
 
@@ -32,20 +29,23 @@ itaee <- read_csv("../data/bie/processed/itaee.csv") %>%
   
 
 
-estado_ae <- read_csv("../data/resultados/integrado/por_estado.csv") %>% 
+estado_ae <- read_csv("selecto_estado.csv" %>% 
+    file.path("../data/resultados/integrado/", .)) %>%
   select(trimestre, Estado, CVEENT, acteco)
 
 junta_columnas <- c("trimestre", "CVEENT", "Estado")
-estado_totales <- pibe %>% 
+estado_totales_ <- pibe %>% 
   right_join(itaee, by = junta_columnas) %>% 
-  inner_join(estado_ae, by = junta_columnas) %>% 
+  inner_join(estado_ae, by = junta_columnas)
+
+estado_totales <- estado_totales_ %>% 
   inner_join(by = c("trimestre", "CVEENT"), metros_ae %>% 
       group_by(trimestre, CVEENT) %>% 
       summarize(por_metros = sum(acteco, na.rm=T))) %>% 
-  inner_join(by = c("trimestre", "CVEENT"), muns_ae %>% 
-      mutate(CVEENT = str_sub(CVEMUN, 1, 2)) %>% 
-      group_by(trimestre, CVEENT) %>% 
-      summarize(por_muns = sum(acteco, na.rm=T))) %>% 
+  # inner_join(by = c("trimestre", "CVEENT"), muns_ae %>% 
+  #     mutate(CVEENT = str_sub(CVEMUN, 1, 2)) %>% 
+  #     group_by(trimestre, CVEENT) %>% 
+  #     summarize(por_muns = sum(acteco, na.rm=T))) %>% 
   group_by(CVEENT, Estado) %>% 
   mutate(es_2014 = (trimestre == "2014-12-01"), 
       itaee_al_pib = itaee/itaee[es_2014]*pibe[es_2014])
@@ -61,43 +61,45 @@ gg_sanity <- estado_totales %>%
     ggtitle("Actividad Económica") +
     scale_x_date("") +
     scale_y_continuous("", labels = . %>% divide_by(1e3)) + 
-  # coord_cartesian(xlim = c("2013-10-01", "2015-10-01") %>% as.Date) +
+  # coord_cartesian(xlim = c("2013-10-01", "2015-10-01") %>% 
+  #     as.Date) +
     scale_color_brewer(palette = "Dark2") +
-    expand_limits(y = 0) +
-    theme(axis.text.x = element_text(angle=45, hjust=1), 
-          legend.position = c(5/7, 1/10))
-
+    # expand_limits(y = 0) +
+    theme(legend.position = c(5/7, 1/10))
 print(gg_sanity)
-ggsave(plot = gg_sanity, "../visualization/comprobación_modelo_35.png",
+ggsave(plot = gg_sanity, 
+  "../visualization/comprobación_modelo_m5.png",
   width = 16, height = 9, dpi = 100)
 
 
-data_met <- read_csv("../data/conapo/conapo_municipio.csv") %>% 
-  select(CVEMUN = cvegeo, fecha, total) %>% 
-  filter(fecha <= "2016-01-01") %>% 
-  mutate(trimestre = fecha %>% ymd %>% add(months(11))) %>% 
-  right_join(metros_ref %>% select(-CVEENT), by = "CVEMUN") %>% 
-  group_by(CVEMET, zona_metro, trimestre) %>% 
-  summarize(poblacion = sum(total, na.rm=T)) %>% ungroup %>% 
-  mutate(CVEMET = str_pad(CVEMET, 3, "left", "0")) %>% 
-  inner_join(metros_ae, by=c("trimestre", "CVEMET", "zona_metro")) %>% 
-  group_by(CVEMET, zona_metro) %>% arrange(trimestre) %>% 
-  mutate(acteco_crec = acteco/lag(acteco) - 1, 
-      acteco_percap  = acteco/poblacion)
+## Ésta no está efectiva. 
 
-gg_acteco_vars <- data_met %>% 
-  filter(trimestre == "2015-12-01") %>% 
-  ggplot(aes(acteco_percap, acteco_crec, color = zona_metro)) + 
-    geom_point() +
-    # geom_text_repel(aes(label = zona_metro)) + 
-    scale_color_manual(values = brewer.pal(8, "Dark2") %>% rep(10)) +
-    theme(legend.position = "none")
-
-print(gg_acteco_vars)
-
-ggsave(plot = gg_acteco_vars, 
-  "../visualization/figures/acteco_transform_.png", 
-  width = 16, height = 9, dpi = 100)
+# data_met <- read_csv("../data/conapo/conapo_municipio.csv") %>% 
+#   select(CVEMUN = cvegeo, fecha, total) %>% 
+#   filter(fecha <= "2016-01-01") %>% 
+#   mutate(trimestre = fecha %>% ymd %>% add(months(11))) %>% 
+#   right_join(metros_ref %>% select(-CVEENT), by = "CVEMUN") %>% 
+#   group_by(CVEMET, zona_metro, trimestre) %>% 
+#   summarize(poblacion = sum(total, na.rm=T)) %>% ungroup %>% 
+#   mutate(CVEMET = str_pad(CVEMET, 3, "left", "0")) %>% 
+#   inner_join(metros_ae, 
+#       by=c("trimestre", "CVEMET", "zona_metro")) %>% 
+#   group_by(CVEMET, zona_metro) %>% arrange(trimestre) %>% 
+#   mutate(acteco_crec = acteco/lag(acteco) - 1, 
+#       acteco_percap  = acteco/poblacion)
+# gg_acteco_vars <- data_met %>% 
+#   filter(trimestre == "2015-12-01") %>% 
+#   ggplot(aes(acteco_percap, acteco_crec, color = zona_metro)) + 
+#     geom_point() +
+#     # geom_text_repel(aes(label = zona_metro)) + 
+#     scale_color_manual(values = brewer.pal(8, "Dark2") %>% rep(10)) +
+#     theme(legend.position = "none")
+# 
+# print(gg_acteco_vars)
+# 
+# ggsave(plot = gg_acteco_vars, 
+#   "../visualization/figures/acteco_transform_.png", 
+#   width = 16, height = 9, dpi = 100)
 
 
 
